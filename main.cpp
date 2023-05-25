f0c56f82c3886912bf9408fa53e45d1629a8da76
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-05-25 12:10:53 +0400

    9) Создание функции Print() для класса Road
    Исправлен length на lenght

--- main.cpp
+++ main.cpp
@@ -3,20 +3,25 @@ using namespace std;
 
 class Road {
 	private:
-	int length;
+	int lenght;
 	int width;
-public:
-Road(int newlength, int newWidth) {
-	length = newlength;
-	width = newWidth;
-}
+	public:
+	Road(int newlenght, int newWidth) {
+		lenght = newlenght;
+		width = newWidth;
+	}
+	void Print() {
+		setlocale(LC_ALL, "ru");
+		cout << "Длина дороги: " << lenght << endl;
+		cout << "Ширина дороги: " << width << endl;
+	}
 };
 
 class Car {
 	private:
 	double X, V;
 	int P;
-public:
+	public:
 	Car(double newX, int newP, double newV) {
 		X = newX;
 		P = newP;
